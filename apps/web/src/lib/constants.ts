10359ac2fa31181e59a24c5f481a056ee693167d
Harsh Thakur <harshprojects2002@outlook.com>
2026-05-25 13:40:08 +0530

    feat(web): scaffold file upload UI and enable remote functions

---
+++ apps/web/src/lib/constants.ts
@@ -0,0 +1,3 @@
+export const MAX_SIZE_MB = '10 MB';
+export const VALID_TYPES = ['PNG', 'JPEG', 'WebP'];
+export const VALID_ACCEPT = 'image/png,image/jpeg,image/webp';
--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	/* eslint-disable @typescript-eslint/no-unused-vars */
+	import { VALID_ACCEPT } from '$lib/constants';
+
+	let selected_file = $state<File | null>(null);
+	let uploading = $state<boolean>(false);
+
+	function handleFileSelect(event: Event) {
+		const input = event.target as HTMLInputElement;
+		const selected_files = input.files![0] ?? null;
+	}
+</script>
+
+<div class="min-h-screen flex items-center justify-center max-w-4xl mx-auto">
+	<div>
+		<!-- Choose file -->
+		<input
+			id="file-input"
+			type="file"
+			accept={VALID_ACCEPT}
+			class="hidden"
+			onchange={handleFileSelect}
+		/>
+		<label for="file-input" class="border-2 px-4 py-1 rounded-sm cursor-pointer inline-block transition hover:scale-110 text-sm">
+			{selected_file ? selected_file.name : "Choose File"}
+		</label>
+	</div>
+	<!-- Upload button -->
+	 {#if selected_file}
+		<button type="button">{uploading ? "uploading...": "upload"}</button>
+	 {/if}
+</div>
